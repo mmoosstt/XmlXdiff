2976ffc177f86815dc2cfaa5a8c99297c3214bf0
Suman Nandan <suman.nandan@gmail.com>
2013-08-13 02:23:56 -0700

    Initial support of attributes.

--- xdiff_core.py
+++ xdiff_core.py
@@ -19,9 +19,9 @@ class Xmldiff:
             return ''
         elif elem.nodeType == minidom.Node.ATTRIBUTE_NODE:
             return self.signature (elem.ownerElement) + '/' + elem.nodeName + '/#attr'
-        elif elem.nodeType != minidom.Node.TEXT_NODE:
-            return self.signature(elem.parentNode) + '/' + elem.nodeName
         else:
+            if elem.parentNode == None:
+                print elem.nodeName, elem.nodeValue
             return self.signature(elem.parentNode) + '/' + elem.nodeName
 
     @classmethod
@@ -35,33 +35,41 @@ class Xmldiff:
         return True
 
     @classmethod
+    def setParentToElem(self, a, p):
+        a.parentNode = p
+
+    @classmethod
+    def child_nodes (self, node):
+        if node.hasChildNodes():
+            for child in node.childNodes:
+                if self.valid_node (child):
+                    self.setParentToElem (child, node)
+                    yield child
+
+        if node.nodeType != minidom.Node.TEXT_NODE and node.hasAttributes:
+            attrmap = node.attributes
+            if attrmap != None:
+                for v in attrmap._attrs.values():
+                    self.setParentToElem (v, node)
+                    yield v
+
+    @classmethod
     def iterleafnodes (self, elem):
-        '''
-        Returns all the leaf level nodes of a subtree rooted at 'elem'
-        That includes all the text nodes and all the attributes.
-        It filter outs all the elements which are not valid node.
-        '''
         leafnodes = []
+        haschild  = False
 
-        if elem.nodeType != minidom.Node.TEXT_NODE and elem.hasAttributes:
-            attrmap = elem.attributes
-            for v in attrmap._attrs.values():
-                leafnodes.append (v)
+        for child in self.child_nodes (elem):
+            haschild = True
+            leafnodes.extend (self.iterleafnodes (child))
 
-        if elem.hasChildNodes():
-            for child in elem.childNodes:
-                if Xmldiff.valid_node (child):
-                    leafnodes.extend (self.iterleafnodes (child))
-        else:
+        if haschild == False:
             leafnodes.append (elem)
+
         return leafnodes
+
 
     @classmethod
     def parents (self, nodes):
-        '''
-        for each element in the set of 'nodes', return the set
-        of their parents.
-        '''
         p = set()
 
         for node in nodes:
@@ -73,30 +81,27 @@ class Xmldiff:
                 p.add (node.parentNode)
         return p
 
-    @classmethod
-    def powerset(self, s):
-        x = len(s)
-        for i in range (1 << x):
-            yield [s[j] for j in range(x) if (i & (1 << j))]
 
     @classmethod
     def isleafnode (self, node):
-        if node == None:
-            return False
+        retVal = True
 
-        if node.hasChildNodes():
-            return False
+        if node == None:
+            retVal =  False
         else:
-            return True
+            for child in self.child_nodes (node):
+                retVal = False;
+                break
+
+        return retVal
 
     @classmethod
     def totalNodes (self, node):
         total = 0
         if node != None:
             total = 1
-            if node.hasChildNodes():
-                for child in [i for i in node.childNodes if self.valid_node(i)]:
-                    total = total + self.totalNodes (child)
+            for child in self.child_nodes (node):
+                total = total + self.totalNodes (child)
         return total
 
     @classmethod
@@ -106,6 +111,7 @@ class Xmldiff:
     @classmethod
     def costDelete (self, node):
         return self.totalNodes (node)
+
 
     def __init__ (self):
         self.dict1     = {}
@@ -123,33 +129,26 @@ class Xmldiff:
             return
 
         # [1]. Create a hash object
-        sha = hashlib.sha1()
+        sha       = hashlib.sha1()
+        isleaf    = True
 
-        # [2]. Look for attributes
-        if elem.nodeType != minidom.Node.TEXT_NODE and elem.hasAttributes:
-            attrmap = elem.attributes
-            for v in attrmap._attrs.values():
-                sha.update (v.nodeName)
-                sha.update (v.value)
+        childhash = []
+        for child in self.child_nodes (elem):
+            isleaf = False
+            childhash.append (self.computehash (child, hashdict))
 
-        # [3]. Look for child nodes
-        if not elem.hasChildNodes():
-            # this somehow does not work with Python installed at SNPS
-            #elem.normalize()
-
+        if isleaf == True:
             value = elem.nodeValue
-
             sha.update (value.strip())
-            hashdict[elem] = sha.hexdigest()
 
         else:
-            childhash = []
-            for child in [i for i in elem.childNodes if self.valid_node(i)]:
-                childhash.append (self.computehash (child, hashdict))
+            for it in sorted (childhash):
+                sha.update (it)
+            sha.update(elem.nodeName)
 
-                for it in sorted (childhash):
-                    sha.update (it)
-            hashdict[elem] = sha.hexdigest()
+        hashdict[elem] = sha.hexdigest()
+        
+        #print 'hash of ', self.signature(elem), ' is ', hashdict[elem]
 
         return hashdict[elem]
 
@@ -177,10 +176,9 @@ class Xmldiff:
 
 
     def computemindist(self, x, y):
-        c1 = [i for i in x.childNodes if self.valid_node(i)]
-        c2 = [i for i in y.childNodes if self.valid_node(i)]
+        c1 = [i for i in self.child_nodes (x)]
+        c2 = [i for i in self.child_nodes (y)]
 
-        # generate partial bipartite matching between childNodes
         (unmappedX, mapped, unmappedY) = self.getPartialBipartMatch (c1, c2)
         dist = 0
         for childx in unmappedX:
@@ -207,44 +205,25 @@ class Xmldiff:
 
 
     def updatedisttable(self, x, y, dist):
+        #print 'setting dist ', x.nodeName, y.nodeName, dist
         self.disttable[(x, y)] =  dist
 
+
     def computedist (self, x, y):
-        # leaf node
-        if self.isleafnode(x) and self.isleafnode(y):
-            if self.signature(x) == self.signature(y):
-                if x.nodeValue == y.nodeValue:  # values same. dist zero
+        #print 'computing distance (', x.nodeName, ',', x.nodeValue,')','(', y.nodeName, y.nodeValue, ')'
+
+        if self.signature(x) == self.signature(y):
+            if self.isleafnode(x) and self.isleafnode(y):
+                if x.nodeValue == y.nodeValue:
                     self.updatedisttable(x, y, 0)
                 else:
                     self.updatedisttable(x, y, 1)
-                self.M_min[(x, y)]     = set([(x, y)])
-
-        if self.isleafnode(x) and y == None:
-            self.updatedisttable(x, y, 1)
-
-        if x == None and self.isleafnode(y):
-            self.updatedisttable(x, y, 1)
-
-        # one is leaf node and another is non leaf node
-        if self.isleafnode(x) and not self.isleafnode(y):
-            self.updatedisttable(x, y, self.costDelete(x) + self.costInsert(y))
-
-        if not self.isleafnode(x) and self.isleafnode(y):
-            self.updatedisttable(x, y, self.costDelete(x) + self.costInsert(y))
-
-        # none leaf node
-        if not self.isleafnode(x) and y == None:
-            self.updatedisttable(x, y, self.costDelete(x))
-
-        elif x == None and not self.isleafnode(y):
-            self.updatedisttable(x, y, self.costInsert(y))
-
-        elif not self.isleafnode(x) and not self.isleafnode(y):
-            if self.signature(x) != self.signature(y):
-                self.updatedisttable(x, y, self.costDelete(x) + self.costInsert(y))
+                self.M_min[(x, y)] = set([(x, y)])
 
             else:
                 self.computemindist(x, y)
+
+                
 
     def mincostmatching (self, root1, root2):
         '''
@@ -259,12 +238,16 @@ class Xmldiff:
         for leaf2 in self.iterleafnodes (root2):
             n2.add (leaf2)
 
+
         while len(n1) != 0 or len(n2) != 0:
+            #print 'n1 = ', [n.nodeName for n in n1]
+            #print 'n2 = ', [n.nodeName for n in n2]
             for x in n1:
                 for y in n2:
                     self.computedist (x, y)
             n1 = self.parents (n1)
             n2 = self.parents (n2)
+
 
     def notPresentInFst (self, mminset, node):
         for (x, y) in mminset:
@@ -279,31 +262,35 @@ class Xmldiff:
         return True
 
     def generatescript(self, root1, root2):
+        # print '---------------------------------'
+        # for (k1, k2) in self.M_min.keys():
+        #     print '[', k1.nodeName, k2.nodeName, '] --> ', [(n1.nodeName, n2.nodeName) for (n1, n2) in self.M_min[(k1, k2)]]
+        # print '---------------------------------'
+ 
         if  (root1, root2) not in self.M_min.keys():
             print "Delete ", root1.nodeName, "Insert ", root2.nodeName
         elif (root1, root2) not in self.M_min[(root1, root2)]:
             print "Delete ", root1.nodeName, "Inset ", root2.nodeName
         else:
-            if (root1, root2) in self.disttable.keys():
-                if self.disttable[(root1, root2)] == 0:
-                    pass
+            if ((root1, root2) in self.disttable.keys()) and (self.disttable[(root1, root2)] == 0):
+                pass
+            else:
+                for x in self.child_nodes (root1):
+                    for y in self.child_nodes (root2):
+                        if (x, y) in self.M_min[(root1, root2)]:
+                            if self.isleafnode(x) and self.isleafnode(y):
+                                if (x, y) in self.disttable.keys():
+                                    if self.disttable[(x, y)] == 0:
+                                        pass
+                                    else:
+                                        print "update ", x.nodeValue, " to ", y.nodeValue
+                            else: #x, y are not leaf nodes
+                                self.generatescript(x, y)
 
-            for x in [i for i in root1.childNodes if self.valid_node(i)]:
-                for y in [i for i in root2.childNodes if self.valid_node(i)]:
-                    if (x, y) in self.M_min[(root1, root2)]:
-                        if self.isleafnode(x) and self.isleafnode(y):
-                            if (x, y) in self.disttable.keys():
-                                if self.disttable[(x, y)] == 0:
-                                    pass
-                                else:
-                                    print "update ", x.nodeValue, " to ", y.nodeValue
-                        else: #x, y are not leaf nodes
-                            self.generatescript(x, y)
-
-                    if self.notPresentInSnd (self.M_min[(root1, root2)], y):
-                        print "insert ", y.toxml()
-                if self.notPresentInFst (self.M_min[(root1, root2)], x):
-                    print "delete ", x.toxml()
+                        if self.notPresentInSnd (self.M_min[(root1, root2)], y):
+                            print "insert ", self.signature(y) #y.toxml()
+                    if self.notPresentInFst (self.M_min[(root1, root2)], x):
+                        print "delete ", self.signature(x) #x.toxml()
 
     def xdiff(self):
         # check and filtering
@@ -316,6 +303,7 @@ class Xmldiff:
             # generate editscript
             self.generatescript (self.root1, self.root2)
 
+
 def _test_signature(file):
     doc1 = minidom.parse(file)
     root1 = doc1.documentElement
@@ -324,17 +312,8 @@ def _test_signature(file):
         # [1]. print self
         print Xmldiff.signature (elem)
 
-        # [2]. print attributes
-        if elem.nodeType != minidom.Node.TEXT_NODE and elem.hasAttributes:
-            attrmap = elem.attributes
-            for v in attrmap._attrs.values():
-                print Xmldiff.signature(v)
-
-        # [3]. print children
-        if elem.hasChildNodes():
-            for child in elem.childNodes:
-                if Xmldiff.valid_node (child):
-                    inner_test (child)
+        for child in Xmldiff.child_nodes (elem):
+            inner_test (child)
 
     inner_test (root1)
 
@@ -342,9 +321,9 @@ import graph
 
 
 if __name__ == '__main__':
-    # print "testing xdiff_core..."
-    # xd = Xmldiff();
-    # xd.readxml('tests/test3/a.xml', 'tests/test3/b.xml')
-    # xd.xdiff()
+    print "testing xdiff_core..."
+    xd = Xmldiff();
+    xd.readxml('tests/test3/a.xml', 'tests/test3/b.xml')
+    xd.xdiff()
     
-    _test_signature('tests/test3/a.xml')
+    # _test_signature('tests/test3/a.xml')
