66eebc6a3bef5ecfae6c8efdfd075ad54a540154
morit <morit@192.168.0.2>
2018-12-09 18:23:24 +0100

    moved to class structure

--- lxmldiff/xdiffCore.py
+++ lxmldiff/xdiffCore.py
@@ -1,129 +1,145 @@
 import lxmldiff
 import lxml.etree
 import hashlib
-import xml.etree
-
-xml.etree
-
-_path1 = "{}\\tests\\test1\\a.xml".format(lxmldiff.getPath())
-_path2 =  "{}\\tests\\test1\\b.xml".format(lxmldiff.getPath())
-
-_xml1 = lxml.etree.parse(_path1)
-_xml2 = lxml.etree.parse(_path2)
-
-_root1 = _xml1.getroot()
-_root2 = _xml2.getroot()
 
 
-def customElementHashAll(element, hashpipe):
-  
-    hashpipe.update(bytes(str(element.tag), 'utf-8'))
-    
-    if hasattr(element, "attrib"):
-        for _name in sorted(element.attrib.keys()):
-            _attrib_value = element.attrib[_name]
-            hashpipe.update(bytes(_name + _attrib_value, 'utf-8'))
-            
-    if element.text is not None:
-        hashpipe.update(bytes(element.text.strip(),'utf-8'))
-    
-    if element.tail is not None:
-        hashpipe.update(bytes(element.tail.strip(), 'utf-8'))
-            
-    for child in element.getchildren():
-        hashpipe.update(customElementHashAll(child, hashpipe))
+class xDiff(object):
+
+    @classmethod
+    def callbackHashAll(cls, element, hashpipe):
+      
+        hashpipe.update(bytes(str(element.tag)  + "#tag", 'utf-8'))
         
-        
-    return bytes(hashpipe.hexdigest(), 'utf-8')
-
-def customElementHashIgnoreTag(element, hashpipe):
-    # check path consentiently  and attribute consentiently
-    
-    hashpipe.update(bytes(str(element.tag), 'utf-8'))
-    
-    if hasattr(element, "attrib"):
-        for _name in sorted(element.attrib.keys()):
-            _attrib_value = element.attrib[_name]
-            #hashpipe.update(bytes(_name + _attrib_value, 'utf-8'))
-            hashpipe.update(bytes(_name, 'utf-8'))
-            
-    #if element.text is not None:
-    #    #hashpipe.update(bytes(element.text.strip(),'utf-8'))
-    
-    #if element.tail is not None:
-    #    #hashpipe.update(bytes(element.tail.strip(), 'utf-8'))
-            
-    for child in element.getchildren():
-        hashpipe.update(customElementHashIgnoreTag(child, hashpipe))
-        
-        
-    return bytes(hashpipe.hexdigest(), 'utf-8')
-        
-
-
-def getChildren(xml, element, pathes, hashes):
-    
-    _hash = hashlib.sha1()
-    _hash.update(lxml.etree.tostring(element))
-    _path = xml.getpath(element)
-    pathes[_path] = _hash
-    hashes[_hash.hexdigest()] = _path
-    
-    for _child in element.getchildren():
-        getChildren(xml, _child, pathes, hashes)
-        
-
-           
-def getChildren2(xml, element, pathes, hashes, callbackHashCalculation):
-    
-    _hash = hashlib.sha1()
-    callbackHashCalculation(element, _hash)
-    _path = xml.getpath(element)
-    
-    pathes[_path] = _hash.hexdigest()
-    
-    if _hash.hexdigest() not in hashes.keys():
-        hashes[_hash.hexdigest()] = (_path,)
-    else:
-        hashes[_hash.hexdigest()] += (_path,)
-    
-    
-    for _child in element.getchildren():
-        getChildren2(xml, _child, pathes, hashes, callbackHashCalculation)
-              
-_pathes1 = {}
-_hashes1 = {}
-_pipe = ()
-getChildren2(_xml1, _root1,  _pathes1, _hashes1, customElementHashAll)
-
-_pathes2 = {}
-_hashes2 = {}
-_pipe = ()
-getChildren2(_xml2, _root2,  _pathes2, _hashes2, customElementHashAll)
-
-
-def report(hashes1, hashes2):
-    for _hash1 in hashes1.keys():
-        _pathes1 = hashes1[_hash1]
-        
-        for _path1 in _pathes1:
-            if _hash1 in hashes2.keys():
-                _pathes2 = hashes2[_hash1]
+        if hasattr(element, "attrib"):
+            for _name in sorted(element.attrib.keys()):
+                _attrib_value = element.attrib[_name]
+                hashpipe.update(bytes(_name + _attrib_value + "#att", 'utf-8'))
                 
-                for _path2 in _pathes2:
-                    if _path1 == _path2:
-                        print('element unchanged, path unchanged', _path1, _path2)
-                    else:
-                        print('element unchanged, path   changed', _path1, _path2)
-            else:
-                print('element   changed,                    ', _path1)
+        if element.text is not None:
+            hashpipe.update(bytes(element.text.strip() + "#txt",'utf-8'))
+        
+        if element.tail is not None:
+            hashpipe.update(bytes(element.tail.strip() + "#txt", 'utf-8'))
+                
+        for child in element.getchildren():
+            hashpipe.update(cls.callbackHashAll(child, hashpipe))
+            
+            
+        return bytes(hashpipe.hexdigest(), 'utf-8')
+
+    @classmethod
+    def callbackHashValueConsitency(cls, element, hashpipe):
+             
+        if hasattr(element, "attrib"):
+            for _name in sorted(element.attrib.keys()):
+                _attrib_value = element.attrib[_name]
+                hashpipe.update(bytes(_attrib_value + "#att", 'utf-8'))
+                
+        if element.text is not None:
+            hashpipe.update(bytes(element.text.strip() + "#txt",'utf-8'))
+        
+        if element.tail is not None:
+            hashpipe.update(bytes(element.tail.strip() + "#txt", 'utf-8'))
+                
+        for child in element.getchildren():
+            hashpipe.update(cls.callbackHashAll(child, hashpipe))
+            
+            
+        return bytes(hashpipe.hexdigest(), 'utf-8')
+    
+    @classmethod
+    def callbackHashGrammerConsitency(cls, element, hashpipe):
+        # check path consentiently  and attribute consentiently
+        
+        hashpipe.update(bytes(str(element.tag) + "#tag", 'utf-8'))
+        
+        if hasattr(element, "attrib"):
+            for _name in sorted(element.attrib.keys()):
+                hashpipe.update(bytes(_name + "#tag", 'utf-8'))
+                
+                
+        for child in element.getchildren():
+            hashpipe.update(cls.callbackHashGrammerConsitency(child, hashpipe))
+            
+            
+        return bytes(hashpipe.hexdigest(), 'utf-8')
+        
+
+    @classmethod
+    def getHashesElementBased(cls, xml, element, hashes):
+        
+        _hash = hashlib.sha1()
+        _hash.update(lxml.etree.tostring(element))
+        _path = xml.getpath(element)
+        #pathes[_path] = _hash
+        hashes[_hash.hexdigest()] = _path
+        
+        for _child in element.getchildren():
+            cls.getHashesElementBased(xml, _child, hashes)
+        
+
+
+    @classmethod           
+    def getHashesElementBasedCustomised(cls, xml, element, hashes, callbackHashCalculation):
+        
+        _hash = hashlib.sha1()
+        callbackHashCalculation(element, _hash)
+        _path = xml.getpath(element)
+        
+        #pathes[_path] = _hash.hexdigest()
+        
+        if _hash.hexdigest() not in hashes.keys():
+            hashes[_hash.hexdigest()] = [_path]
+        else:
+            hashes[_hash.hexdigest()].append(_path)
+        
+        
+        for _child in element.getchildren():
+            cls.getHashesElementBasedCustomised(xml, _child, hashes, callbackHashCalculation)
+            
+class xDiffExecutor(object):
+      
+    def __init__(self):
+        self.path1 = "{}\\tests\\test1\\a.xml".format(lxmldiff.getPath())
+        self.path2 =  "{}\\tests\\test1\\b.xml".format(lxmldiff.getPath())
+
+        self.xml1 = lxml.etree.parse(self.path1)
+        self.xml2 = lxml.etree.parse(self.path2)
+
+        self.root1 = self.xml1.getroot()
+        self.root2 = self.xml2.getroot()
+
+        self.hashes1 = {}
+        self.hashes2 = {}
+        
+        xDiff.getHashesElementBasedCustomised(self.xml1, self.root1,  self.hashes1, xDiff.callbackHashAll)
+        xDiff.getHashesElementBasedCustomised(self.xml2, self.root2,  self.hashes2, xDiff.callbackHashAll)
+        
+        
+        self.dev1()
+        
+
+    def dev1(self):
+        for _hash1 in self.hashes1.keys():
+            _pathes1 = self.hashes1[_hash1]
+            
+            for _path1 in _pathes1:
+                if _hash1 in self.hashes2.keys():
+                    _pathes2 = self.hashes2[_hash1]
+                    
+                    for _path2 in _pathes2:
+                        if _path1 == _path2:
+                            print('element unchanged, path unchanged', _path1, _path2)
+                        else:
+                            print('element unchanged, path   changed', _path1, _path2)
+                else:
+                    print('element   changed,                    ', _path1)
            
-print('source -> target') 
-report(_hashes1, _hashes2)
-
-print('target -> source') 
-report(_hashes2, _hashes1)
 
 
 
 
+
+if __name__ == "__main__":
+    _x = xDiffExecutor()
+    
