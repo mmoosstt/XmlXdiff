484de32aa70912736a236775a2b8a2eacb8e95b0
morit <morit@192.168.0.2>
2018-12-09 09:20:56 +0100

    Custom Element Hash added

--- lxmldiff/xdiffCore.py
+++ lxmldiff/xdiffCore.py
@@ -1,6 +1,9 @@
 import lxmldiff
 import lxml.etree
 import hashlib
+import xml.etree
+
+xml.etree
 
 _path1 = "{}\\tests\\test1\\a.xml".format(lxmldiff.getPath())
 _path2 =  "{}\\tests\\test1\\b.xml".format(lxmldiff.getPath())
@@ -10,6 +13,30 @@ _xml2 = lxml.etree.parse(_path2)
 
 _root1 = _xml1.getroot()
 _root2 = _xml2.getroot()
+
+
+def customElementHash(element, hashpipe):
+  
+    hashpipe.update(bytes(str(element.tag), 'utf-8'))
+    
+    if hasattr(element, "attrib"):
+        for _name in sorted(element.attrib.keys()):
+            _attrib_value = element.attrib[_name]
+            hashpipe.update(bytes(_name + _attrib_value, 'utf-8'))
+            
+    if element.text is not None:
+        hashpipe.update(bytes(element.text.strip(),'utf-8'))
+    
+    if element.tail is not None:
+        hashpipe.update(bytes(element.tail.strip(), 'utf-8'))
+            
+    for child in element.getchildren():
+        hashpipe.update(customElementHash(child, hashpipe))
+        
+        
+    return bytes(hashpipe.hexdigest(), 'utf-8')
+        
+
 
 def getChildren(xml, element, pathes, hashes):
     
@@ -23,43 +50,28 @@ def getChildren(xml, element, pathes, hashes):
         getChildren(xml, _child, pathes, hashes)
         
 
-def updatePipe(element):
-    pipe += (element.tag,)
-    if hasattr(element, "attrib"):
-        for _name in sorted(element.attrib.keys()):
-            _attrib_value = element.attrib[_name]
-            pipe += (_name, _attrib_value,)
-            
-    if element.text is not None:
-        pipe += (element.text.strip(), )
+           
+def getChildren2(xml, element, pathes, hashes):
     
-    if element.tail is not None:
-        pipe += (element.tail.strip(), )
-            
-    return pipe
-            
-def getChildren2(xml, element, pipe, pathes, hashes):
-    
-    pipe = updatePipe(pipe, element)
-       
     _hash = hashlib.sha1()
-    _hash.update(str(pipe).encode('utf-8'))
+    customElementHash(element, _hash)
     _path = xml.getpath(element)
-    pathes[_path] = _hash
+    pathes[_path] = _hash.hexdigest()
     hashes[_hash.hexdigest()] = _path
     
+    
     for _child in element.getchildren():
-        getChildren2(xml, _child, pipe,  pathes, hashes)
+        getChildren2(xml, _child, pathes, hashes)
               
 _pathes1 = {}
 _hashes1 = {}
 _pipe = ()
-getChildren2(_xml1, _root1, _pipe, _pathes1, _hashes1)
+getChildren2(_xml1, _root1,  _pathes1, _hashes1)
 
 _pathes2 = {}
 _hashes2 = {}
 _pipe = ()
-getChildren2(_xml2, _root2, _pipe, _pathes2, _hashes2)
+getChildren2(_xml2, _root2,  _pathes2, _hashes2)
 
 
 def report(hashes1, hashes2):
@@ -70,14 +82,18 @@ def report(hashes1, hashes2):
             _path2 = hashes2[_hash1]
             
             if _path1 == _path2:
-                print('eleeme unchanged, element unchanged', _path1, _path2)
+                print('element unchanged, element unchanged', _path1, _path2)
             else:
-                print('path changed,   element unchanged', _path1, _path2)
+                print('path      changed,   element unchanged', _path1, _path2)
         else:
-            print('element changed                 ', _path1)
+            print('element   changed,                    ', _path1)
            
 print('source -> target') 
 report(_hashes1, _hashes2)
 
 print('target -> source') 
 report(_hashes2, _hashes1)
+
+
+
+
