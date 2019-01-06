c0f3d133df374bdc9bace4461970f43f7e00106d
morit <morit@192.168.0.2>
2019-01-06 13:26:04 +0100

    execute scripts added

---
+++ execute_tests.py
@@ -0,0 +1,34 @@
+import unittest
+import sys
+import os
+import io
+
+sys.stdout = io.StringIO()
+
+sys.path.append(os.path.abspath("./tests"))
+from GeneralTests import CompareAll
+
+test_suite = unittest.TestSuite()
+test_suite.addTest(CompareAll('test1'))
+test_suite.addTest(CompareAll('test2'))
+test_suite.addTest(CompareAll('test3'))
+test_suite.addTest(CompareAll('test4'))
+test_suite.addTest(CompareAll('test5'))
+test_suite.addTest(CompareAll('test6'))
+test_suite.addTest(CompareAll('test7'))
+test_suite.addTest(CompareAll('test8'))
+test_suite.addTest(CompareAll('test9'))
+test_suite.addTest(CompareAll('test11'))
+
+test_runner = unittest.TextTestRunner(verbosity=2)
+res = test_runner.run(test_suite)
+
+sys.stdout.flush()
+sys.stdout.seek(0)
+
+sys.stderr.flush()
+sys.stderr.seek(0)
+
+f = open('out.txt', 'w')
+f.write(sys.stdout.read())
+f.close()
