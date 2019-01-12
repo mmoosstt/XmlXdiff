8c62d8c61bca36ee18f439528c787dcb0e191b55
morit <morit@192.168.0.2>
2019-01-12 21:42:58 +0100

    hashlib prepared to ignore children

--- lib/XmlXdiff/XHash.py
+++ lib/XmlXdiff/XHash.py
@@ -11,11 +11,12 @@ import hashlib
 class XDiffHasher(object):
 
     @classmethod
-    def callbackHashAll(cls, element, hashpipe):
+    def callbackHashAll(cls, element, hashpipe, children=True):
 
         _element_childes = element.getchildren()
-        for child in _element_childes:
-            hashpipe.update(cls.callbackHashAll(child, hashpipe))
+        if children:
+            for child in _element_childes:
+                hashpipe.update(cls.callbackHashAll(child, hashpipe))
 
         hashpipe.update(bytes(str(element.tag) + '#tag', 'utf-8'))
         # attributes and text are only taken into account for leaf nodes
@@ -35,12 +36,13 @@ class XDiffHasher(object):
         return bytes(hashpipe.hexdigest(), 'utf-8')
 
     @classmethod
-    def callbackHashAttributeValueElementValueConsitency(cls, element, hashpipe):
+    def callbackHashAttributeValueElementValueConsitency(cls, element, hashpipe, children=True):
 
         _element_childes = element.getchildren()
-        for child in _element_childes:
-            hashpipe.update(
-                cls.callbackHashAttributeValueElementValueConsitency(child, hashpipe))
+        if children:
+            for child in _element_childes:
+                hashpipe.update(
+                    cls.callbackHashAttributeValueElementValueConsitency(child, hashpipe))
 
         if hasattr(element, 'attrib'):
             for _name in sorted(element.attrib.keys()):
@@ -56,12 +58,13 @@ class XDiffHasher(object):
         return bytes(hashpipe.hexdigest(), 'utf-8')
 
     @classmethod
-    def callbackHashTagNameAttributeNameValueConsitency(cls, element, hashpipe):
+    def callbackHashTagNameAttributeNameValueConsitency(cls, element, hashpipe, children=True):
 
         _element_childes = element.getchildren()
-        for child in _element_childes:
-            hashpipe.update(
-                cls.callbackHashTagNameAttributeNameValueConsitency(child, hashpipe))
+        if children:
+            for child in _element_childes:
+                hashpipe.update(
+                    cls.callbackHashTagNameAttributeNameValueConsitency(child, hashpipe))
 
         hashpipe.update(bytes(str(element.tag) + '#tag', 'utf-8'))
         if hasattr(element, 'attrib'):
@@ -72,12 +75,13 @@ class XDiffHasher(object):
         return bytes(hashpipe.hexdigest(), 'utf-8')
 
     @classmethod
-    def callbackHashTagNameAttributeNameConsitency(cls, element, hashpipe):
+    def callbackHashTagNameAttributeNameConsitency(cls, element, hashpipe, children=True):
 
         _element_childes = element.getchildren()
-        for child in _element_childes:
-            hashpipe.update(
-                cls.callbackHashTagNameAttributeNameConsitency(child, hashpipe))
+        if children:
+            for child in _element_childes:
+                hashpipe.update(
+                    cls.callbackHashTagNameAttributeNameConsitency(child, hashpipe))
 
         hashpipe.update(bytes(str(element.tag) + '#tag', 'utf-8'))
 
@@ -88,22 +92,24 @@ class XDiffHasher(object):
         return bytes(hashpipe.hexdigest(), 'utf-8')
 
     @classmethod
-    def callbackHashTagNameConsitency(cls, element, hashpipe):
+    def callbackHashTagNameConsitency(cls, element, hashpipe, children=True):
 
         _element_childes = element.getchildren()
-        for child in _element_childes:
-            hashpipe.update(cls.callbackHashTagNameConsitency(child, hashpipe))
+        if children:
+            for child in _element_childes:
+                hashpipe.update(
+                    cls.callbackHashTagNameConsitency(child, hashpipe))
 
         hashpipe.update(bytes(str(element.tag) + '#tag', 'utf-8'))
 
         return bytes(hashpipe.hexdigest(), 'utf-8')
 
     @classmethod
-    def getHashes(cls, xelements, callbackHashAlgorithm):
+    def getHashes(cls, xelements, callbackHashAlgorithm, children=True):
 
         for _xelement in xelements:
 
             _hash_algo = hashlib.sha1()
-            callbackHashAlgorithm(_xelement.node, _hash_algo)
+            callbackHashAlgorithm(_xelement.node, _hash_algo, children)
             _hash = _hash_algo.hexdigest()
             _xelement.setHash(_hash)
